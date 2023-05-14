0aa64ed08e748c143d6bd549e829aa5bb3ba54f0
johanneswittenburg <johanneswittenburg@gmail.com>
2023-05-14 18:14:33 +0200

    add axios

--- src/CharacterGallery.tsx
+++ src/CharacterGallery.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import GalleryCard from "./GalleryCard";
+import axios from "axios";
 
 type Character = {
     id: number
@@ -11,10 +12,33 @@ type Props = {
     characters: Character[]
 }
 function CharacterGallery(props: Props) {
+
+    const [response, setResponse] = useState<Character[]>([])
+    const [searchTerm, setSearchTerm] = useState<string>(" ")
+    const filteredChars = props.characters.filter((character) => character.name.includes(searchTerm))
+
+    function getAllCharactersFromApi(){
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((api) => {
+                setResponse(api.data.results);
+            })
+    }
+
+    useEffect(getAllCharactersFromApi, [])
+
+
+    function onChangeInputUser(event: ChangeEvent<HTMLInputElement>){
+        setSearchTerm(event.target.value);
+    }
+
     return (
+        <>
+            <input placeholder={"search..."} onChange={onChangeInputUser}/>
+            <button onClick={getAllCharactersFromApi}>get chars</button>
         <div>
             {props.characters.map(thisCharacter => <GalleryCard key={thisCharacter.id} character={thisCharacter}/>)}
         </div>
+        </>
     );
 }
 
